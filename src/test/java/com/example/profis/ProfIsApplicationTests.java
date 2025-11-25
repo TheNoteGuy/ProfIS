618d079a2e44e63d3911096ba19b878a734cdf7c
NoteGuy <nico.nickel2005@gmail.com>
2025-11-25 16:40:48 +0100

    Springboot Setup2

---
+++ src/test/java/com/example/profis/ProfIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
